4a13c3d3161f3e86ae09ca6e3b9e76a2a074f147
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-20 20:40:28 +0530

    Update afk.py

--- Hero/modules/afk.py
+++ Hero/modules/afk.py
@@ -4,10 +4,10 @@ from telegram import Message, Update, Bot, User
 from telegram import MessageEntity
 from telegram.ext import Filters, MessageHandler, run_async
 
-from tg_bot import dispatcher
-from tg_bot.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
-from tg_bot.modules.sql import afk_sql as sql
-from tg_bot.modules.users import get_user_id
+from Hero import dispatcher
+from Hero.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
+from Hero.modules.sql import afk_sql as sql
+from Hero.modules.users import get_user_id
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
