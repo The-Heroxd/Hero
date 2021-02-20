65226f6d58aca1d36376addaa8df4c82f1bbb033
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-20 21:16:39 +0530

    Update muting.py

--- Hero/modules/muting.py
+++ Hero/modules/muting.py
@@ -7,11 +7,11 @@ from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import mention_html
 
-from tg_bot import dispatcher, LOGGER
-from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
-from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
-from tg_bot.modules.helper_funcs.string_handling import extract_time
-from tg_bot.modules.log_channel import loggable
+from Hero import dispatcher, LOGGER
+from Hero.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
+from Hero.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from Hero.modules.helper_funcs.string_handling import extract_time
+from Hero.modules.log_channel import loggable
 
 
 @run_async
