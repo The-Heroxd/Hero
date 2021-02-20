f1b418060609bcf49cd1fddd7137c83805e85888
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-20 20:33:26 +0530

    Update and rename sample_config.py to Hero/sample_config.py

--- Hero/sample_config.py
+++ Hero/sample_config.py
@@ -11,8 +11,8 @@ class Config(object):
 
     # REQUIRED
     API_KEY = ""
-    OWNER_ID = "683538773" # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "ViruZs"
+    OWNER_ID = "1633862409" # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "me_iz_zero"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
