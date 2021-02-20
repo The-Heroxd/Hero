58fc5b72a2e53f0cf2f89afee22948bb7b82c518
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-20 20:45:32 +0530

    Update connection.py

--- Hero/modules/connection.py
+++ Hero/modules/connection.py
@@ -7,15 +7,15 @@ from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import mention_html
 
-import tg_bot.modules.sql.connection_sql as sql
-from tg_bot import dispatcher, LOGGER, SUDO_USERS
-from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
-from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
-from tg_bot.modules.helper_funcs.string_handling import extract_time
+import Hero.modules.sql.connection_sql as sql
+from Hero import dispatcher, LOGGER, SUDO_USERS
+from Hero.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
+from Hero.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from Hero.modules.helper_funcs.string_handling import extract_time
 
-# from tg_bot.modules.translations.strings import tld
+# from Hero.modules.translations.strings import tld
 
-from tg_bot.modules.keyboard import keyboard
+from Hero.modules.keyboard import keyboard
 
 @user_admin
 @run_async
