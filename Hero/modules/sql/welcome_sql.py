4ee1feefb71d9c365a46953b4b4490898555897b
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-20 21:24:20 +0530

    Update and rename welcome_sql.py to Hero/modules/sql/welcome_sql.py

--- Hero/modules/sql/welcome_sql.py
+++ Hero/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
 
-from tg_bot.modules.helper_funcs.msg_types import Types
-from tg_bot.modules.sql import SESSION, BASE
+from Hero.modules.helper_funcs.msg_types import Types
+from Hero.modules.sql import SESSION, BASE
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
 DEFAULT_GOODBYE = "Nice knowing ya!"
