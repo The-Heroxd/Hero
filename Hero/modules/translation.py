cd2f23d05ee0b4811c10651a619ba3a4b79fddfe
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-20 21:11:27 +0530

    Update translation.py

--- Hero/modules/translation.py
+++ Hero/modules/translation.py
@@ -6,7 +6,7 @@ import requests
 from telegram import Update, Bot
 from telegram.ext import CommandHandler
 
-from tg_bot import dispatcher
+from Hero import dispatcher
 
 # Open API key
 API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
