9d859bdc6e7d1aee0f8cecaca10d67da1386e280
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-20 21:08:44 +0530

    Update userinfo.py

--- Hero/modules/userinfo.py
+++ Hero/modules/userinfo.py
@@ -6,10 +6,10 @@ from telegram import ParseMode, MAX_MESSAGE_LENGTH
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown
 
-import tg_bot.modules.sql.userinfo_sql as sql
-from tg_bot import dispatcher, SUDO_USERS
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.extraction import extract_user
+import Hero.modules.sql.userinfo_sql as sql
+from Hero import dispatcher, SUDO_USERS
+from Hero.modules.disable import DisableAbleCommandHandler
+from Hero.modules.helper_funcs.extraction import extract_user
 
 
 @run_async
