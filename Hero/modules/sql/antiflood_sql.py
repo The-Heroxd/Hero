d56a8cedd4ff3b14f95bb5f2dc3ea8c7b828359b
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-21 12:19:20 +0530

    Update antiflood_sql.py

--- Hero/modules/sql/antiflood_sql.py
+++ Hero/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer
 
-from tg_bot.modules.sql import SESSION, BASE
+from Hero.modules.sql import SESSION, BASE
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
