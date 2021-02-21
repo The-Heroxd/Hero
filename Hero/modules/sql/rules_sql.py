8de5bc499dd799b7a6a9dbbb6d655bb46f273321
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-21 12:34:43 +0530

    Update rules_sql.py

--- Hero/modules/sql/rules_sql.py
+++ Hero/modules/sql/rules_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from tg_bot.modules.sql import SESSION, BASE
+from Hero.modules.sql import SESSION, BASE
 
 
 class Rules(BASE):
