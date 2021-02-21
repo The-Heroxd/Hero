439ef17a71d49f25c6c342d13a2f814dd2b808a0
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-21 12:41:48 +0530

    Update misc.py

--- Hero/modules/helper_funcs/misc.py
+++ Hero/modules/helper_funcs/misc.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode
 from telegram.error import TelegramError
 
-from tg_bot import LOAD, NO_LOAD
+from Hero import LOAD, NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
