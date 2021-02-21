382d8b00a99521d7b7509a2a8aa9857bc4cce27e
The-Heroxd <79360643+The-Heroxd@users.noreply.github.com>
2021-02-21 12:20:35 +0530

    Update connection_sql.py

--- Hero/modules/sql/connection_sql.py
+++ Hero/modules/sql/connection_sql.py
@@ -3,8 +3,8 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, func, distinct
 
-from tg_bot.modules.helper_funcs.msg_types import Types
-from tg_bot.modules.sql import SESSION, BASE
+from Hero.modules.helper_funcs.msg_types import Types
+from Hero.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
